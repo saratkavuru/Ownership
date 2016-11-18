5965560eb361cc0ec9fbab7409b523f615fb615a
saratkavuru <kavurusarat@gmail.com>
2016-11-18 03:13:08 -0500

    Feeble attempts at chat and private pub

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,35 @@
+class ConversationsController < ApplicationController
+  before_filter :authenticate_user!
+
+
+  def create
+    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+
+    render json: { conversation_id: @conversation.id }
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+    @reciever = interlocutor(@conversation)
+    @messages = @conversation.messages
+    @message = Message.new
+  end
+
+  def index
+    @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
+    @conversations = Conversation.involving(current_user).order("created_at DESC")
+  end
+
+  private
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
+
+  def interlocutor(conversation)
+    current_user == conversation.recipient ? conversation.sender : conversation.recipient
+  end
+end
